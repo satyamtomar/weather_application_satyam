041d3a43bd4e5f79ef6ffa964f130ee0034b4ee0
Satyam Tomar <saitamaitachi123@gmail.com>
2022-10-29 21:31:23 +0530

    second

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const path = require("path");
 const requests = require("requests");
 // const we = require('../public/public_main.js')
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 // const public_main = require('../public/public_main.js')
 var six_days_weather = [];
@@ -14,9 +14,6 @@ app.set("view engine", "hbs");
 
 app.get("/", (req, res) => {
   res.render("index");
-});
-app.get("/city", (req, res) => {
-  // console.log(we);
 });
 
 app.listen(port, () => {
